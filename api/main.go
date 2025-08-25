56dfcc28725d9e48b817f2349b23a4cacd3f8f22
Diego Oliveira <diegoitarare@gmail.com>
2025-08-25 20:29:33 -0300

    chore: dockerize project

---
+++ api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/api/v1/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	})
+
+	addr := ":8787"
+	log.Println("API rodando em", addr)
+	log.Fatal(http.ListenAndServe(addr, r))
+}
